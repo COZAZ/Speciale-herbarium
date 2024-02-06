a670860e79e398a674fb25468ea240aeb5805640
ContonJon <jonathanstenkilde@hotmail.com>
2024-02-06 14:56:38 +0100

    Added image conversion to array

--- herbariumtest.py
+++ herbariumtest.py
@@ -1,13 +1,11 @@
 import requests
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
 from io import BytesIO
 
-all_data = pd.read_csv("Herb.csv")
 data_jpg = pd.read_csv("Herb.csv", usecols=[16])
-
-print(data_jpg.values[0])
 
 # Extract the string from the list
 image_url = data_jpg.values[0][0]
@@ -17,8 +15,14 @@ response = requests.get('https://specify-attachments.science.ku.dk/static/NHMD_B
 
 # Check if the request was successful
 if response.status_code == 200:
-    # Open and display the image using PIL
+
     img = Image.open(BytesIO(response.content))
-    img.show()
+
+    # Convert image to greyscale and numpy array
+    img_gray = img.convert('L')
+    img_array = np.array(img_gray)
+
+    plt.imshow(img_array, cmap="gray")
+    plt.show()
 else:
     print("Failed to retrieve the image.")
