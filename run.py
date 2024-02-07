939d8bcbf32d65788a34d2560dcb3ffdb5588b82
ContonJon <jonathanstenkilde@hotmail.com>
2024-02-07 14:31:37 +0100

    Data set is ready

---
+++ run.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+test_image = plt.imread("../herb_images/im_1.jpg") # Update path to your own data set location
+
+plt.imshow(test_image, cmap="gray")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+### Pipeline steps begin here??? ###
