63f419332feb80615eb12d8a3a1aaa88bbba5820
Frederik Safft <frederiksafft@hotmail.com>
2024-02-13 14:17:19 +0100

    added helperfunc for run and ocr also in

---
+++ helperfuncs.py
@@ -0,0 +1,9 @@
+import cv2
+
+def resize_image(image, scale=35):
+    scale_percent = scale  # percent of original size
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    return resized_image
--- run.py
+++ run.py
@@ -5,8 +5,9 @@ import easyocr
 import cv2
 
 from label_extractor import getNineLabels, runAgain
+from helperfuncs import resize_image
 
-test_image_path = "exp5"
+test_image_path = "exp104"
 
 ### LETS GET THE TXT ###
 parent_directory = "runs"
@@ -18,7 +19,6 @@ runAgain(doImages=False)
 # FOLDER_PATH = KUN KØR FOR EN I STEDET FOR ALLE, F.EKS. folder_path=test_image_path
 # Vil du køre alle billederne skal du bare ikke specificere folder_path
 data_with_digit_9 = getNineLabels(parent_directory, folder_path=test_image_path)
-print(data_with_digit_9)
 
 for data in data_with_digit_9:
     image_path = 'herb_images/' + data[1]
@@ -50,14 +50,24 @@ for data in data_with_digit_9:
     # Draw bounding box on the image
     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-    # Resize the image
-    scale_percent = 10  # percent of original size
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    resized_image = resize_image(image, scale=10)
 
     # Display the resized image with the bounding box using OpenCV
     cv2.imshow('Image with Bounding Box', resized_image)
+    cv2.waitKey(0)  # Wait until a key is pressed
+    cv2.destroyAllWindows()  # Close the OpenCV window after a key is pressed
+
+    # Crop the image
+    cropped_image = image[y1:y2, x1:x2]
+
+    # Perform OCR on the cropped image
+    reader = easyocr.Reader(['en', 'da'])
+    result = reader.readtext(cropped_image, detail=0)
+    print(result)
+    
+    resized_cropped_image = resize_image(cropped_image, scale=35)
+
+    # Show the cropped image
+    cv2.imshow('Cropped Image', resized_cropped_image)
     cv2.waitKey(0)  # Wait until a key is pressed
     cv2.destroyAllWindows()  # Close the OpenCV window after a key is pressed
