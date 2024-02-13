dff55132716e52a4b6763f05ef71fdde5e382e48
Frederik Safft <frederiksafft@hotmail.com>
2024-02-13 13:40:54 +0100

    removed files

--- tensorflowhelper.py
+++
@@ -1,9 +0,0 @@
-from yolov5.detect import run, parse_opt, main
-
-run(
-    weights="MELU-Trained-ObjDetection-Model-Yolov5-BEST.pt",
-    source="herb_images/im_64.jpg",
-    conf_thres=0.4,
-    imgsz=(416, 416)
-)
-
