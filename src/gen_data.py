696350dd12008c64f0bd92f0e12e144743c57c36
OliverCJuhl <olivercjuhl@gmail.com>
2024-03-26 14:27:54 +0100

    Small update to gen_data.

--- src/gen_data.py
+++ src/gen_data.py
@@ -1,3 +1,5 @@
 from BERT.text_data_synthesizer import synthesize_text_data, pretty_print_text_data
 
 generated_bert_text = synthesize_text_data(1000, asJson=True)
+
+print("Generated text data with {0} samples".format(len(generated_bert_text)))
