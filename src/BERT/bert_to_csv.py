05248cdfde2184ae28dac623354e3f3546a930e3
ContonJon <jonathanstenkilde@hotmail.com>
2024-05-07 12:53:13 +0200

    More post-processing for names

--- src/BERT/bert_to_csv.py
+++ src/BERT/bert_to_csv.py
@@ -80,6 +80,9 @@ def createCSV():
             if det_calc > det_score:
                 det_score = det_calc
                 det_correct_index = j
+            # If Det never found in OCR reading
+            if len(pred_det) == 0:
+                det_score = 100
 
             date_calc = fuzz.ratio(text_piece, pred_date)
             if date_calc > date_score:
@@ -140,19 +143,34 @@ def createCSV():
         if leg_score >= threshold:
             leg_csv = ocr_object[leg_correct_index]
             leg_csv = cleanName("LEG", ocr_object, leg_csv)
+            leg_csv = findAndFormatLeg(leg_csv)
+            
+            det_index = leg_csv.upper().find("DET")
+            if det_index != -1:
+                leg_csv = leg_csv[:det_index]
+                
+            leg_csv = remove_digits(leg_csv)
+            leg_csv = remove_date_month(leg_csv)
+            leg_csv = ' '.join(leg_csv.split())
+
+            if "LEG" not in leg_csv.upper():
+                leg_csv = "Leg: " + leg_csv
 
             data[i+1][3] = leg_csv
 
         if det_score >= threshold:
             det_csv = ocr_object[det_correct_index]
-            det_csv_2 = cleanName("DET", ocr_object, det_csv)
+            det_csv = cleanName("DET", ocr_object, det_csv)
+            det_csv = ' '.join(det_csv.split())
 
-            # Use the Leg name if there are no Det names (because then Leg and Det are the same)
-            if ((det_csv == det_csv_2) and ("DET" not in det_csv_2.upper())) or (len(det_csv_2.replace(" ", "")) <= 6):
-                data[i+1][4] = data[i+1][3]
+            if "DET" not in det_csv.upper() or (pred_det == "") or ("DET" in det_csv.upper() and len(det_csv) <= 7):
+               data[i+1][4] = findAndFormatDet(data[i+1][3])
             else:
-                data[i+1][4] = det_csv
-
+                data[i+1][4] = findAndFormatDet(det_csv)
+            
+            if len(data[i+1][3]) <= 7:
+                data[i+1][3] = data[i+1][4].replace("Det", "Leg")
+                
         if date_score >= threshold:
             date_csv = ocr_object[date_correct_index]
             data[i+1][5] = date_csv
@@ -196,12 +214,67 @@ def cleanName(name_type, ocr_obj, name_text):
         for text in ocr_obj:
             if name_type in text.upper() and len(text.replace(" ", "")) > 6:
                 name_text = text
-                break
+                break            
         
     if name_type in name_text.upper():
-            # Find the index of "Loc"
+            # Find the index of "Det/Leg"
             index = name_text.upper().find(name_type)
-            # Extract the substring starting from the index of "Loc"
+            # Extract the substring starting from the index of "Leg/Det"
             name_text = name_text[index:]
     
     return name_text
+
+def findAndFormatLeg(text):
+    #Find first match for "Leg" and then filters out the next characters until an UPPER CHARACTER is reached (beginning of name)
+    match = re.search(r'Leg[^A-Z]*(?P<first_upper>[A-Z]).*', text)
+    if match:
+        extracted_text = "Leg: " + match.group('first_upper') + re.sub(r'\s+', ' ', match.group()[(match.start('first_upper')+1):])
+        return extracted_text
+    else: return text
+
+def findAndFormatDet(text):
+    #Find first match for "Leg" and then filters out the next characters until an UPPER CHARACTER is reached (beginning of name)
+    #Breakdown:
+    #r'Leg searches for the pattern 'Leg'
+    #[^A-Z] searches for any characters that are NOT uppercase - ^ is a negation * gobbles it up
+    #?^<first_upper> is a capturing group which captures the first upper case letter
+    #.* gobbles up all characters after the first upper case letter after 'Leg'
+    #match.group('first_upper'): This retrieves the uppercase letter captured by the named group "first_upper" in the regular expression.
+    #match.group()[(match.start('first_upper')+1):]: This retrieves the substring following the first uppercase letter in the matched text. (match.start('first_upper')+1) gets the index position of the character following the first uppercase letter, and match.group()[(match.start('first_upper')+1):] retrieves the substring from that index position to the end of the matched text.
+
+    #Match for 'Leg' (uppercase L)
+    match = re.search(r'Leg[^A-Z]*(?P<first_upper>[A-Z]).*', text)
+    #Match for 'Det' (uppercase D)
+    match2 = re.search(r'Det[^A-Z]*(?P<first_upper>[A-Z]).*', text)
+    #Match for 'det' (lowercase D) 
+    match3 = re.search(r'det[^A-Z]*(?P<first_upper>[A-Z]).*', text) #:OOOOO
+    if match:
+        extracted_text = "Det: " + match.group('first_upper') + match.group()[(match.start('first_upper')+1):]
+        return extracted_text
+    if match2:
+        extracted_text2 = "Det: " + match2.group('first_upper') + match2.group()[(match2.start('first_upper')+1):]
+        return extracted_text2
+    if match3:
+        extracted_text3 = "Det: " + match3.group('first_upper') + match3.group()[(match3.start('first_upper')+1):]
+        return extracted_text3
+    else: return text
+    
+def remove_digits(input_string):
+    result = ''
+    for char in input_string:
+        if (not char.isdigit()) and ('/' != char):
+            result += char
+
+    return result
+
+def remove_date_month(input_string):
+    # Regex that removes every instance of the string "date" no matter case
+    pattern = re.compile("date", re.IGNORECASE)
+    input_string = pattern.sub("", input_string)
+    
+    # Regex that removes every instance of a month as a string no matter case
+    month_list = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    # Added word boundaries to the regex. This means that for example if a string contains a month as a substring it won't be deleted
+    month = re.compile(r'\b(?:%s)\b' % '|'.join(month_list), re.IGNORECASE)
+
+    return month.sub("", input_string)
